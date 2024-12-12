d0f41c53a7b0a5960c2ead2becaba0d316876244
Miriam Gomez <miriamalejandrago@gmail.com>
2024-12-12 11:55:35 -0500

    intial commit

---
+++ src/main/java/com/pluralsight/dealership/APICarDealership/ApiCarDealershipApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.dealership.APICarDealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarDealershipApplication.class, args);
+	}
+
+}
